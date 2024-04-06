5f3b9370dbea56b55b2ff3cd72e28e217af510ad
Djuliatama <dwina147@gmail.com>
2024-04-06 23:07:44 +0700

    first commit

---
+++ BEJ 11-binar gold-Dije.js
@@ -0,0 +1,22 @@
+class Employee {
+    sayHello(name) {
+        console.info(`Hello ${name} , my name is employee ${this.name}`);
+     }
+}
+    
+class Manager extends Employee {
+    //sayHello(name) {
+    //    console.info(`Hello ${name} , my name is manager ${this.name}`);
+    // }
+}
+
+const dije = new Employee();
+dije.name = "Dije";
+dije.sayHello("John");
+
+const john = new Manager();
+john.name = "John";
+john.sayHello("John");
+
+console.info(dije);
+console.info(john);
